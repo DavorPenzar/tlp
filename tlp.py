5d67b618ed337b62cea15be3fe35f7a74ce9f89a
Davor Penzar <davor.penzar@gmail.com>
2019-06-04 16:25:49 +0200

    Točka na kraju stavke u literaturi

--- tlp.py
+++ tlp.py
@@ -25,7 +25,7 @@ from tlp import *
 
 1.  <a class=\"anchor\" id=\"bib-dunlavy-10\"></a> D. M. Dunlavy, T. G. Kolda, E.
     Acar, *Temporal Link Prediction using Matrix and Tensor Factorizations*,
-    2010, arXiv: [1005.4006 [math.NA]](http://arxiv.org/abs/1005.4006)
+    2010, arXiv: [1005.4006 [math.NA]](http://arxiv.org/abs/1005.4006).
 
 """
 
